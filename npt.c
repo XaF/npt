83df2739c13c37a5a50bdc9684fb307b96e1c896
Raphaël Beamonte <raphael.beamonte@gmail.com>
2012-09-22 16:01:23 -0400

    Add histogram and standard deviation statistic
    Signed-off-by: Raphaël Beamonte <raphael.beamonte@gmail.com>

--- npt.c
+++ npt.c
@@ -16,17 +16,24 @@
 #include <string.h>
 #include <inttypes.h>
 #include <getopt.h>
+#include <math.h>
 
 #include <config.h>
 
-#ifdef NPT_TRACE
-#	ifdef NPT_LTTNG_UST
+#if NPT_TRACE == 1
+#	if NPT_LTTNG_UST == 1
 #		define TRACEPOINT_DEFINE
 #		include "ust_npt.h"
-#	endif /* NPT_LTTNG_UST */
-//#	define TRACE_SESSION_NAME "npt"
-//#	include <lttng/lttng.h>
-#endif
+#	else /* NPT_LTTNG_UST == 1 */
+#		undef NPT_LTTNG_UST
+#	endif /* NPT_LTTNG_UST == 1 */
+#	if NPT_LTTNG_KERNEL != 1
+#		undef NPT_LTTNG_KERNEL
+#	endif /* NPT_LTTNG_KERNEL != 1 */
+#else /* NPT_TRACE == 1 */
+#	undef NPT_LTTNG_UST
+#	undef NPT_LTTNG_KERNEL
+#endif /* NPT_TRACE == 1 */
 
 /**
  * Create a structure to store the variables
@@ -36,11 +43,12 @@ struct globalArgs_t {
 	int verbosity;		/* -v option */
 #	endif /* NPT_ALLOW_VERBOSITY */
 
-	uint64_t loops;	/* -l option */
-	bool trace_ust;
-	bool trace_kernel;
-	int picoseconds; /* flag */
-	int nanoseconds; /* flag */
+	uint64_t loops;		/* -l option */
+	char* output;		/* -o option */
+	bool trace_ust;		/* -t option */
+	bool trace_kernel;	/* -t option */
+	int picoseconds;	/* flag */
+	int nanoseconds;	/* flag */
 	
 	unsigned long cpuHz;
 	double cpuPeriod;
@@ -55,6 +63,7 @@ void initopt() {
 #	endif /* NPT_ALLOW_VERBOSITY */
 
 	globalArgs.loops = NPT_DEFAULT_LOOP_NUMBER;
+	globalArgs.output = NULL;
 	globalArgs.trace_ust = false;
 	globalArgs.trace_kernel = false;
 	globalArgs.picoseconds = false;
@@ -121,9 +130,9 @@ int npt_getopt(int argc, char **argv) {
 		int option_index = 0;
 
 #		ifdef NPT_ALLOW_VERBOSITY		
-		char* shortopt = {"l:t:v"};
+		char* shortopt = {"l:o:t:v"};
 #		else /* NPT_ALLOW_VERBOSITY */
-		char* shortopt = {"l:t:"};
+		char* shortopt = {"l:o:t:"};
 #		endif /* NPT_ALLOW_VERBOSITY */
 
 		c = getopt_long(argc, argv, shortopt,
@@ -152,7 +161,10 @@ int npt_getopt(int argc, char **argv) {
 			
 			// Option --output (-o)
 			case 'o':
-				
+				if (asprintf(&globalArgs.output, "%s", optarg) < 0) {
+					fprintf(stderr, "--output: argument invalid\n");
+					return 1;
+				}
 				break;
 			
 			// Option --trace (-t)
@@ -233,9 +245,17 @@ int cycle() {
 	double duration = 0;
 	volatile uint64_t counter = 0;
 	uint64_t t0, t1;
+	
+	// General statistics
 	double minDuration = 99999.0;
 	double maxDuration = 0.0;
 	double sumDuration = 0.0;
+	
+	// For variance and standard deviation
+	double deltaDuration = 0.0;
+	double meanDuration = 0.0;
+	double meanSquared = 0.0;
+	double variance_n, stdDeviation;
 	
 	// Time declaration for the first loop
 	t0 = rdtsc();
@@ -246,20 +266,31 @@ int cycle() {
 	while (counter < globalArgs.loops+NPT_NOCOUNTLOOP) {
 		// Calculate diff between t0 and t1
 		duration = diff(t1, t0);
+
+#		ifdef NPT_LTTNG_UST
+		tracepoint(ust_npt, nptloop, counter, duration);
+#		endif /* NPT_LTTNG_UST */
+
 		
+		// Increment counter as we have done one more cycle
+		counter++;
+
 		// Update stat variables
-		if (counter > NPT_NOCOUNTLOOP-1) {
+		if (counter > NPT_NOCOUNTLOOP) {
+			// General statistics
 			if (duration < minDuration) minDuration = duration;
 			if (duration > maxDuration) maxDuration = duration;
 			sumDuration += duration;
+			
+			// For variance and standard deviation
+			deltaDuration = duration - meanDuration;
+			meanDuration = meanDuration + deltaDuration / (counter-NPT_NOCOUNTLOOP);
+			meanSquared = meanSquared + deltaDuration * (duration - meanDuration);
 			
 			// Store data in the histogram if duration < max size
 			if (duration < NPT_HISTOGRAM_SIZE) histogram[(int)duration]++;
 			else histogramOverruns++;
 		}
-		
-		// Increment counter as we have done one more cycle
-		counter++;
 		
 		// Get new t0 from rdtsc function
 		t1 = t0;
@@ -267,30 +298,54 @@ int cycle() {
 	}
 	// Readapt the counter to the number of counted cycles
 	counter = counter-NPT_NOCOUNTLOOP;
+	
+	// Calcul of variance dans standard deviation
+	variance_n = meanSquared / counter;
+	stdDeviation = sqrt(variance_n);
 
 	// Print the statistics
 	printf("%" PRIu64 " cycles done over %" PRIu64 ".\n", counter, globalArgs.loops);
 	printf("Cycles duration:\n");
-	printf("	min:	%f %s\n", minDuration, UNITE(globalArgs.picoseconds, globalArgs.nanoseconds));
-	printf("	max:	%f %s\n", maxDuration, UNITE(globalArgs.picoseconds, globalArgs.nanoseconds));
-	printf("	mean:	%f %s\n", sumDuration / (double)counter, UNITE(globalArgs.picoseconds, globalArgs.nanoseconds));
-	printf("	sum:	%f %s\n", sumDuration, UNITE(globalArgs.picoseconds, globalArgs.nanoseconds));
+	printf("	min:		%f %s\n", minDuration, UNITE(globalArgs.picoseconds, globalArgs.nanoseconds));
+	printf("	max:		%f %s\n", maxDuration, UNITE(globalArgs.picoseconds, globalArgs.nanoseconds));
+	printf("	mean:		%f %s\n", sumDuration / (double)counter, UNITE(globalArgs.picoseconds, globalArgs.nanoseconds));
+	printf("	sum:		%f %s\n", sumDuration, UNITE(globalArgs.picoseconds, globalArgs.nanoseconds));
+	printf("	variance:	%f %s\n", variance_n, UNITE(globalArgs.picoseconds, globalArgs.nanoseconds));
+	printf("	std dev:	%f %s\n", stdDeviation, UNITE(globalArgs.picoseconds, globalArgs.nanoseconds));
 	
 	return 0;
 }
 
 int print_histogram() {
 	int i;
+	FILE *hfd = NULL;
+	
+	if (globalArgs.output != NULL) {
+		hfd = fopen(globalArgs.output, "w");
+		if (hfd == NULL) {
+			fprintf(stderr, "Error: unable to open '%s' in write mode.\n", globalArgs.output);
+			globalArgs.output = NULL;
+		} else {
+			fprintf(hfd, "# Data generated by NPT for %" PRIu64 " cycles\n", globalArgs.loops);
+			fprintf(hfd, "# The time value is expressed in %s.\n", UNITE(globalArgs.picoseconds, globalArgs.nanoseconds));
+			fprintf(hfd, "#\n");
+			fprintf(hfd, "#	time	nb. cycles\n");
+			fprintf(hfd, "#	------------------\n");
+		}
+	}
 	
 	printf("--------------------------\n");
-	printf("duration (%s)	no. cycles\n", UNITE(globalArgs.picoseconds, globalArgs.nanoseconds));
+	printf("duration (%s)	nb. cycles\n", UNITE(globalArgs.picoseconds, globalArgs.nanoseconds));
 	printf("--------------------------\n");
 	for (i = 0; i < NPT_HISTOGRAM_SIZE; i++) {
 		// Just print the lines for which we have data
 		if (histogram[i] > 0) {
 			printf("%d		%" PRIu64 "\n", i, histogram[i]);
+			if (globalArgs.output != NULL)
+				fprintf(hfd, "	%d	%" PRIu64 "\n", i, histogram[i]);
 		}
 	}
+	if (globalArgs.output != NULL) fclose(hfd);
 	printf("--------------------------\n");
 	printf("Overruns (%d+): %" PRIu64 "\n", NPT_HISTOGRAM_SIZE, histogramOverruns);
 	
@@ -313,6 +368,8 @@ int main (int argc, char **argv) {
 	cycle();
 	
 	print_histogram();
+	
+	free(globalArgs.output);
 		
 	return 0;
 }
