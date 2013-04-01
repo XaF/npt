068cae410831b1ec00a94bba8f9d30a0e9bdded4
Raphaël Beamonte <raphael.beamonte@gmail.com>
2013-04-01 16:29:26 -0400

    Add GPL headers to all the project files
    Signed-off-by: Raphaël Beamonte <raphael.beamonte@gmail.com>

--- include/npt/npt.h
+++ include/npt/npt.h
@@ -1,4 +1,25 @@
 /**
+ * Non-Preempt Test (npt) tool
+ * Copyright 2012-2013  Raphaël Beamonte <raphael.beamonte@gmail.com>
+ *
+ * This file is part of Foobar.
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License Version
+ * 2 as published by the Free Software Foundation.
+ *
+ * npt is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ * or see <http://www.gnu.org/licenses/>.
+ */
+
+/**
  * Enable DEBUG messages
  */
 //#define DEBUG
